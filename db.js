562bf1002828dc66c24e5a7b6d319d741cc76a4c
Pratik Thange <pratikbthange@gmail.com>
2026-09-20 09:23:53 +0530

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 //Define the mongoDB Connection URl
 const mongoURL='mongodb://localhost:27017/hotels'  //replace 'hotels' with your database name
-//const mongoURL = 'mongodb://admin:Pratik2005@ac-1lrltd0-shard-00-00.yhcnaxh.mongodb.net:27017,ac-1lrltd0-shard-00-01.yhcnaxh.mongodb.net:27017,ac-1lrltd0-shard-00-02.yhcnaxh.mongodb.net:27017/?ssl=true&replicaSet=atlas-uqpy9v-shard-0&authSource=admin&appName=Cluster0';
+
 //set up MongoDB connection
 
 mongoose.connect(mongoURL);
